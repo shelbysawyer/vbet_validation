bd6a48e38f02c4517647e546f3bd6885df5b5285
Shelby Sawyer <shelbytsawyer@gmail.com>
2021-07-08 15:55:01 -0600

    Updates to Colors and Layouts

--- vbet_validation.R
+++ vbet_validation.R
@@ -1,17 +1,15 @@
 vbet_validation<-read.csv("C:/research/transform_fun/data/vbet_validation.csv")
 
-## Basic box plot-- this works, but it's ugly
-boxplot(vbet_validation$slope)
-
 library(ggplot2)
 library(DBI)
 library(tidyverse)
+library(viridis)
 
 # Creating database
 
 vbet_validation_db <-dbConnect(RSQLite::SQLite(), "vbet_validation.db")
 
-# Creating 'observations' table:
+# Creating observations table:
 
 dbExecute(vbet_validation_db, "CREATE TABLE observations (
           point_id,
@@ -41,15 +39,24 @@ observations
 observations %>%
 ggplot(mapping = aes(x = slope, color = recorder_id)) +
   geom_boxplot() +
-  labs (x = "Slope (degrees)") +
-  theme_minimal()
+  labs (x = "Slope (degrees)")
+
+## I'd like to color code points BY recorder_id.. can't figure out how to do 
+## that in a way that there is a separate color for each instead of a ramp
+observations %>%
+ggplot(mapping = aes(x = slope, y = likelihood, color = recorder_id)) +
+  geom_point () +
+  labs(x = " ", y = "Likelihood", color = "User") +
+  theme_minimal() +
+  theme(legend.position = "right") 
 
 # Histogram for Evidence Rasters
 
 ggplot(data = observations, 
        mapping = aes(x = slope)) +
   geom_histogram(bins = 10) +
-  labs (x = "Slope (degrees)", y = "Count")
+  labs (x = "Slope (degrees)", y = "Count") +
+  theme_light()
 
 ggplot(data = observations, 
        mapping = aes(x = hand)) +
