a6ff49ba1c20c3007e514f63f0c6917221413679
Shelby Sawyer <shelbytsawyer@gmail.com>
2021-07-10 05:45:33 -0600

    Final updates before I leave

--- vbet_validation.R
+++ vbet_validation.R
@@ -1,54 +1,114 @@
-vbet_validation<-read.csv("C:/research/transform_fun/data/vbet_validation.csv")
+# Load data and packages
+evidence <-read.csv("/research/transform_fun/vbet_evidence_01.csv")
 
 library(ggplot2)
 library(DBI)
 library(tidyverse)
-library(viridis)
+library(RSQLite)
 
-# Creating database
+#Change data type of userid to factor
+evidence$userid = as.factor(evidence$userid)
 
-vbet_validation_db <-dbConnect(RSQLite::SQLite(), "vbet_validation.db")
+# Ignore for now. Creating SQL database
+vbet_evidence_db <-dbConnect(RSQLite::SQLite(), "vbet_evidence.db")
 
-# Creating observations table:
-
-dbExecute(vbet_validation_db, "CREATE TABLE observations (
-          point_id,
-          slope,
+# Ignore for now. Creating evidence table
+dbExecute(evidence_db, "CREATE TABLE evidence (
+          observationid,
+          categoryid,
+          userid,
+          huc8,
           hand,
           distance,
           twi,
-          likelihood,
-          recorder_id
+          slope
           );")
 
-# Loading csv into 'observations' table:
-
-observations <- read.csv("data/vbet_validation.csv", 
+# Ignore this for now. Loading csv into 'observations' table:
+observations <- read.csv("data/vbet_evidence_01.csv", 
                     stringsAsFactors = FALSE)
 names(observations)
 
-dbWriteTable(vbet_validation_db, "observations", observations, append = TRUE)
+dbWriteTable(vbet_evidence_db, "evidence", evidence, append = TRUE)
 
 # Checking to make sure it's reading the data properly
 
-observations <- dbGetQuery(vbet_validation_db, "SELECT * FROM observations;")
-observations
+evidence <- dbGetQuery(vbet_evidence_db, "SELECT * FROM evidence;")
+evidence
 
-## Experimenting with color-coding users in the box plot
+# Box plots
 
-observations %>%
-ggplot(mapping = aes(x = slope, color = recorder_id)) +
+evidence %>%
+  ggplot(mapping = aes(x = slope)) +
   geom_boxplot() +
   labs (x = "Slope (degrees)")
 
-## I'd like to color code points BY recorder_id.. can't figure out how to do 
-## that in a way that there is a separate color for each instead of a ramp
-observations %>%
+evidence %>%
+  ggplot(mapping = aes(x = hand)) +
+  geom_boxplot() +
+  labs (x = "HAND (m)")
+
+evidence %>%
+  ggplot(mapping = aes(x = distance)) +
+  geom_boxplot() +
+  labs (x = "Distance from Nearest Drainage Cell")
+
+evidence %>%
+  ggplot(mapping = aes(x = twi)) +
+  geom_boxplot() +
+  labs (x = "Topograpgic Wetness Index")
+
+# Histograms
+
+evidence %>%
+  ggplot(mapping = aes(x = slope)) +
+  geom_histogram() +
+  labs (x = "Slope (degrees)")
+
+evidence %>%
+  ggplot(mapping = aes(x = hand)) +
+  geom_histogram() +
+  labs (x = "HAND (m)")
+
+evidence %>%
+  ggplot(mapping = aes(x = distance)) +
+  geom_histogram() +
+  labs (x = "Distance from Nearest Drainage Cell")
+
+evidence %>%
+  ggplot(mapping = aes(x = twi)) +
+  geom_histogram() +
+  labs (x = "Topograpgic Wetness Index")
+
+# Experimenting with color-coding by users in the box plot.. still working this
+# out
+
+evidence %>%
+  ggplot(mapping = aes(x = slope, y = categoryid)) +
+  geom_point () +
+  labs(x = "Slope", y = "Likelihood", color = "User") +
+  theme_minimal() +
+  theme(legend.position = "right")
+
+evidence %>%
+  ggplot(mapping = aes(x = slope, y = categoryid, color = userid)) +
+  geom_point () +
+  labs(x = "Slope", y = "Likelihood", color = "User") +
+  theme_minimal() +
+  theme(legend.position = "right") 
+
+evidence %>%
+ggplot(mapping = aes(x = slope, color = userid)) +
+  geom_boxplot() +
+  labs (x = "Slope (degrees)")
+
+# Still in progress of converting these over to new column names
+evidence %>%
 ggplot(mapping = aes(x = slope, y = likelihood, color = recorder_id)) +
   geom_point () +
   labs(x = " ", y = "Likelihood", color = "User") +
   theme_minimal() +
-  theme(legend.position = "right") 
+  theme(legend.position = "right")
 
 # Histogram for Evidence Rasters
 
@@ -73,7 +133,30 @@ ggplot(data = observations,
   geom_histogram(bins = 10) +
   labs (x = "Topographic Wetness Index (TWI)", y = "Count")
 
-## The next thing I want to do is create box plots of the slope values
-## but I want to split the plots out by recorder_id (there are 5)
+# Summary statistics
+
+max(evidence$slope)
+min(evidence$slope)
+mean(evidence$slope)
+mode(evidence$slope)
+median(evidence$slope)
+
+max(evidence$hand)
+min(evidence$hand)
+mean(evidence$hand)
+mode(evidence$hand)
+median(evidence$hand)
+
+max(evidence$distance)
+min(evidence$distance)
+mean(evidence$distance)
+mode(evidence$distance)
+median(evidence$distance)
+
+max(evidence$twi)
+min(evidence$twi)
+mean(evidence$twi)
+mode(evidence$twi)
+median(evidence$twi)
 
 
